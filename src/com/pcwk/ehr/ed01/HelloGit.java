27bd39bdd2450f54405fea03278847bcdf841df0
pcwk <hyykim22@gmail.com>
2025-04-21 16:45:43 +0900

    Eclipse commit1

---
+++ src/com/pcwk/ehr/ed01/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 파일명:HelloGit.java <br/>
+ * 생성일:2025-04-21
+ */
+package com.pcwk.ehr.ed01;
+
+/**
+ * 
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
