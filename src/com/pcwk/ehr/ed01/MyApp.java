a161517786a77e678e88e540a319db13877d20a5
pcwk <hyykim22@gmail.com>
2025-04-22 18:26:53 +0900

    dsfa

--- src/com/pcwk/ehr/ed01/MyApp.java
+++ src/com/pcwk/ehr/ed01/MyApp.java
@@ -9,6 +9,6 @@ package com.pcwk.ehr.ed01;
 public class MyApp {
 
 				System.out.println("┌───────────────────────┐");
-				System.out.println("│프로그램 종료               │");
+				System.out.println("│프로그램 종료99               │");
 				System.out.println("└───────────────────────┘");
 }
