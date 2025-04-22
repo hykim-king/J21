72aeed6262258461b630c5a4599f1646e856504f
김현룡 <hykim22@gmail.com>
2025-04-22 18:29:15 +0900

    Update MyApp.java
    d

--- src/com/pcwk/ehr/ed01/MyApp.java
+++ src/com/pcwk/ehr/ed01/MyApp.java
@@ -1,14 +1,14 @@
 /**
- * 파일명:MyApp.java <br/>
- * 생성일:2025-04-17
+ * 파일명:HelloGit.java <br/>
+ * 생성일:2025-04-22
  */
 package com.pcwk.ehr.ed01;
 
+public class HelloGit {
+
+	public static void main(String[] args) {
 
 
-public class MyApp {
+	}
 
-				System.out.println("┌───────────────────────┐");
-				System.out.println("│프로그램 종료99               │");
-				System.out.println("└───────────────────────┘");
 }
