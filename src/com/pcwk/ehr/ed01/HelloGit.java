ed9f5e56361a3171beaed40cd4b299462160bf81
pcwk <hyykim22@gmail.com>
2025-04-22 15:33:13 +0900

    88번째

--- src/com/pcwk/ehr/ed01/HelloGit.java
+++ src/com/pcwk/ehr/ed01/HelloGit.java
@@ -16,6 +16,7 @@ public class HelloGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Hello Git88");
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
 		System.out.println("Hello Git2");
