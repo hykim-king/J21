9ac406f256837243e4d76125e36c062687487ba2
pcwk <hyykim22@gmail.com>
2025-04-22 15:01:35 +0900

    수정

--- src/com/pcwk/ehr/ed06/Ed01ExcelWrite.java
+++ src/com/pcwk/ehr/ed06/Ed01ExcelWrite.java
@@ -4,36 +4,9 @@
  */
 package com.pcwk.ehr.ed06;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Ed01ExcelWrite {
 	public static void main(String[] args) {
-		Workbook workbook = new XSSFWorkbook();
-		Sheet sheet = workbook.createSheet("학생정보");
-
-		Row header = sheet.createRow(0);
-		header.createCell(0).setCellValue("이름");
-		header.createCell(1).setCellValue("나이");
-		header.createCell(2).setCellValue("학년");
-
-		// 데이터 추가
-		Row row1 = sheet.createRow(1);
-		row1.createCell(0).setCellValue("홍길동");
-		row1.createCell(1).setCellValue(20);
-		row1.createCell(2).setCellValue(2);
-		try (FileOutputStream fos = new FileOutputStream("students.xlsx")) {
-			workbook.write(fos);
-			workbook.close();
-			System.out.println("Excel 파일 생성 완료!");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,3 @@
 module J21 {
-	requires org.apache.poi.ooxml;
-	requires org.apache.logging.log4j;
+
 }
