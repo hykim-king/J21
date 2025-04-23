935a6d88a1bf4743e9e16a515ac6004ddda092c8
pcwk <hyykim22@gmail.com>
2025-04-23 09:23:49 +0900

    commit8

---
+++ src/com/pcwk/ehr/ed01/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 파일명:Hello.java <br/>
+ * 생성일:2025-04-23
+ */
+package com.pcwk.ehr.ed01;
+
+/**
+ * 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
